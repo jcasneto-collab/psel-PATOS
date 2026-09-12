b23337eb2f1a6936dc2c5735c5bcbb6d724b1d6c
jcasneto-collab <j.casneto@icloud.com>
2026-09-12 12:40:06 -0300

    salvando o código

--- loadbalancer/main.go
+++ loadbalancer/main.go
@@ -9,6 +9,39 @@ import (
 	"strconv"
 )
 
+func forwardbody (reader *bufio.Reader, connectionBackend net.Conn, contentLength string) error{
+	numbytes, err := strconv.Atoi(contentLength)
+	if err != nil {
+		log.Println("Error while converting")
+		returncd ..
+	}
+	buffer := make([]byte,numbytes)
+	_, err = io.ReadFull(reader, buffer)
+	if err != nil {
+		log.Println("error while take bytes")
+		err = httpcore.SendResponse(connection, 400, "Bad Request", "text/html", "400 Bad Request")
+			if err != nil {
+				log.Println("Error of Bad Requesting")
+				return
+			}
+	}
+	_, err = connectionBackend.Write(buffer)
+	if err != nil {
+		log.Println("error sending resposne response of body")
+		err = httpcore.ParseRequest(connection, 400, "Bad Request", "text/html", "400 Bad Request")
+		if err != nil {
+			log.Println("error converting")
+			return
+		}
+	}
+	_, err = io.Copy(connection, connectionBackend)
+	if err != nil{
+		log.Println("error sending response")
+		return
+	}
+}
+
+
 func forwardToBackend(connection net.Conn, Request httpcore.Request, reader *bufio.Reader) {
 	connectionBackend, err := net.Dial("tcp", "localhost:8001")
 	if err != nil {
@@ -46,8 +79,8 @@ func forwardToBackend(connection net.Conn, Request httpcore.Request, reader *buf
 			log.Println("Error while converting")
 			err = httpcore.SendResponse(connection, 400, "Bad Request", "text/html", "400 Bad Request")
 			if err != nil {
-			log.Println("error converting", err)
-			return
+				log.Println("error converting", err)
+				return
 		}
 		buffer := make([]byte, numbytes)
 		_, err = io.ReadFull(reader, buffer)
@@ -59,26 +92,25 @@ func forwardToBackend(connection net.Conn, Request httpcore.Request, reader *buf
 				log.Println("error of Bad Request")
 				return
 			}
-			return
+			
 		}
+
+	}
 		_, err = connectionBackend.Write(buffer)
 		if err != nil {
 			log.Println("error sending response of body", err)
 			err = httpcore.SendResponse(connection, 400, "Bad Request", "text/html", "400 Bad Request")
 			if err != nil {
-			log.Println("error converting", err)
-			return
-		}
+				log.Println("error converting", err)
+				return
+			}
 
-	}
+		}	
+	
 
 	_, err = io.Copy(connection, connectionBackend)
 	if err != nil {
 		log.Println("error sending response")
-		err = httpcore.SendResponse(connection, 500, "Internal Server Error", "text/html", "500 Internal Server Error")
-		if err != nil {
-			log.Println("error while sending response form backend to client")
-			return
-		}
+		return
 	}
 }
